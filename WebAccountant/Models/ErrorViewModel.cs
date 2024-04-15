511319fe4c6c7e394c19662e964baf8fabc0d9f1
ltvu17 <133189798+ltvu17@users.noreply.github.com>
2024-04-15 09:49:15 +0700

    Add project files.

---
+++ WebAccountant/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAccountant.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
