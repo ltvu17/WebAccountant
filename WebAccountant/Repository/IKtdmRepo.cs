77a6972189e364d824fd0d5a521b15829fc55fad
ltvu17 <133189798+ltvu17@users.noreply.github.com>
2024-04-16 10:21:12 +0700

    Refactor Code

--- WebAccountant/Repository/IKtdmRepo.cs
+++ WebAccountant/Repository/IKtdmRepo.cs
@@ -1,9 +1,13 @@
-﻿using WebAccountant.ModelsBase;
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using WebAccountant.ModelsBase;
 
 namespace WebAccountant.Repository
 {
     public interface IKtdmRepo
     {
         public Task<IEnumerable<Ktdm>> GetAllKtdmsAsync();
+        public Task<EntityEntry<Ktdm>> AddNew(string values);
+        public Task Update(string key, string values);
+        public Task Delete(string key);
     }
 }
