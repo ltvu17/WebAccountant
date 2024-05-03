85b615b6ad14a7689037eb0ea8747eea16a3d80a
ltvu17 <133189798+ltvu17@users.noreply.github.com>
2024-05-03 16:59:30 +0700

    End 5/3/2024

--- WebAccountant/Controllers/KtscsController.cs
+++ WebAccountant/Controllers/KtscsController.cs
@@ -35,13 +35,13 @@ namespace WebAccountant.Controllers
         public async Task<IActionResult> GetDetailPhieuBanHang(int id)
         {
             var phieuBanHang = await _ktscRepo.GetDetailPhieuBanHang(id);
-            return Json(phieuBanHang);
+            return PartialView("~/Views/Home/Pages/Actions/BuyEditPage.cshtml", phieuBanHang);
         }
         [HttpGet]
         public async Task<IActionResult> GetDetailPhieuMuaHang(int id)
         {
-            var phieuBanHang = await _ktscRepo.GetDetailPhieuMuaHang(id);
-            return PartialView("~/Views/Home/Pages/Actions/BuyEditPage.cshtml", phieuBanHang);
+            var phieuMuaHang = await _ktscRepo.GetDetailPhieuMuaHang(id);
+            return PartialView("~/Views/Home/Pages/Actions/BuyEditPage.cshtml", phieuMuaHang);
         }
         [HttpGet]
         public async Task<IActionResult> GetALLDSPhieuMuaHang(DataSourceLoadOptions loadOptions)
